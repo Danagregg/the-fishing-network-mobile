2f2e1cf4e8b7a90a4154fa2bdb853253b066607d
Dana Gregg <danagregg@hotmail.com>
2016-04-23 15:11:29 -0700

    Added more text fields and formatting
    Gross solution. Definitely want to fix.

--- TheFishingNetwork/OperationView.js
+++ TheFishingNetwork/OperationView.js
@@ -20,26 +20,77 @@ export var OperationView = React.createClass ({
 
   getInitialState(){
     return {
-      text:'Hello'
+      fisherName:'Hello',
+      fisherAddress: 'Address',
+      fisherPhone:'905-640',
+      fisherEmail:'hello@gmail.com'
     };
   },
 
+// This is gross, but hackathon yolo
   render() {
     return (
-      <TextInput
-        style={styles.textBox}
-        onChangeText={(text) => this.setState({text})}
-        value={this.state.text}
-      />
+      <View>
+        <View style={styles.textInputs}>
+          <Text style={styles.textLabel}>
+              Fisherman Name
+          </Text>
+          <TextInput
+            style={styles.textBox}
+            onChangeText={(fisherName) => this.setState({fisherName})}
+            value={this.state.fisherName}
+          />
+        </View>
+        <View style={styles.textInputs}>
+          <Text style={styles.textLabel}>
+            Fisherman Address
+          </Text>
+          <TextInput
+            style={styles.textBox}
+            onChangeText={(fisherAddress) => this.setState({fisherAddress})}
+            value={this.state.fisherAddress}
+            />
+        </View>
+        <View style={styles.textInputs}>
+          <Text style={styles.textLabel}>
+            Fisherman Phone Number
+          </Text>
+          <TextInput
+            style={styles.textBox}
+            onChangeText={(fisherPhone) => this.setState({fisherPhone})}
+            value={this.state.fisherPhone}
+          />
+        </View>
+          <View style={styles.textInputs}>
+            <Text style={styles.textLabel}>
+              Fisherman Email
+            </Text>
+          <TextInput
+            style={styles.textBox}
+            onChangeText={(fisherEmail) => this.setState({fisherEmail})}
+            value={this.state.fisherEmail}
+          />
+        </View>
+      </View>
     );
   }
 })
 
 const styles = StyleSheet.create({
+  textInputs: {
+    flexDirection:'row',
+    margin: 10
+  },
+  textLabel: {
+    fontSize:10,
+    fontWeight:'bold',
+    margin: 5,
+    flex:0.3
+  },
   textBox: {
-    margin: 10,
     height: 30,
     borderColor: 'gray',
+    flex: 1,
     borderWidth: 1
   },
 });
